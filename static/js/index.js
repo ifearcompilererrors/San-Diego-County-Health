9a6318021172ae5c66895a6b51dfa2716f289b8d
Angelique De Castro <angeliqueajdecastro@gmail.com>
2017-08-19 16:43:25 -0700

    can you paint with all the colors of your neighbors

--- static/js/index.js
+++ static/js/index.js
@@ -4,14 +4,17 @@ var latlong = [33.026142, -116.696322],
     scale   = $(window).width() * 25,
     popup = new L.Popup({ autoPan: false }),
     centroids = {},
-    geojson,
-    co2Layer,
-    closeTooltip;
+    geojsonLayer = L.layerGroup(),
+    co2Layer = L.layerGroup(),
+    closeTooltip,
+    svg;
 
 // Draw map
 L.mapbox.accessToken = 'pk.eyJ1IjoiaWZlYXJjb21waWxlcmVycm9ycyIsImEiOiJjaXUwOTFpdm0wMXNhMm9xcDZ1MmNiNmF0In0.ZVVzG5Amju9GXPtGPUwEwg';
 var map = L.mapbox.map('map', 'mapbox.streets')
     .setView(latlong, 9);
+
+geojsonLayer.addTo(map)
 
 $('#pollutant-CO2').click(function() {
   if($('#pollutant-CO2').is(':checked') && $('.co2').length == 0) {
@@ -27,10 +30,11 @@ d3.json("/static/json/SRA2010tiger.geojson", function(error, data){
   var neighborhoodBounds = data.features;
 
   // Draw neighborhoods
-  geojson = L.geoJson(neighborhoodBounds, {
+  geojsonLayer.addLayer(
+    L.geoJson(neighborhoodBounds, {
     style: setStyle,
     onEachFeature: collectCentroids
-  }).addTo(map);
+  }));//.addTo(map);
 
   /* Set neighborhood style */
   function setStyle(features) {
@@ -43,6 +47,7 @@ d3.json("/static/json/SRA2010tiger.geojson", function(error, data){
     }
   }
 
+  /* Get coordinates of the center of each neighborhood */
   function collectCentroids(features, layer) {
     centroids[layer.feature.properties.NAME.toLowerCase().replace(/\s+/g, '-')] = 
       $.geo.centroid(layer.feature.geometry).coordinates;
@@ -57,13 +62,14 @@ d3.json("/static/json/SRA2010tiger.geojson", function(error, data){
 
     co2Layer = L.layerGroup();
 
+    // Create emissions layer
     csvdata.forEach(function(d) {
       co2Layer.addLayer(
         L.circle(centroids[d['City'].toLowerCase().replace(/\s+/g, '-')].reverse(), {
           radius: d['Total Reported Emissions']/100,
           className: 'pollutant co2',
         })
-        .on('mouseover', function(e) {
+        .on('mouseover', function(e) { // Add mouseover tooltip
           var layer = e.target,
               data = d;
           var titles = ["<div class='marker-title'>", "Facility: ", "City: " , "State: " , "Total Reported Emissions: " , "Sectors: "]
@@ -107,8 +113,118 @@ d3.json("/static/json/SRA2010tiger.geojson", function(error, data){
           closeTooltip = window.setTimeout(function() {
             map.closePopup();
           }, 100);
-        }) //
-      );
+        }) // end mouseout
+      ); // end addLayer
+    }); // end forEach
+  }); // end emissions csv
+
+  // Dynamically set neighborhood hue based on population
+  d3.csv("/static/csv/2012_San_Diego_Demographics_-_County_Population.csv", function(poperror,popdata){
+    if(poperror) print(poperror)    
+
+    var neighborhood,
+        neighborhoodList,
+        populationAvgs,
+        maxmin, max, min;
+
+    // Calculate averages for each population demographic
+    populationAvgs = getPopAvgs(popdata);
+
+    // Calculate max and min
+    maxmin = getPopMaxMin(popdata);
+
+    // Create color scale
+    svg = d3.select('body').append('svg')
+      .attr('class', 'canvas');
+    colorScale(maxmin[0], maxmin[1], svg);
+
+    // Set neighborhood fill color based on population
+    popdata.forEach(function(d) {
+      neighborhood = d['Area '].toLowerCase().replace(/\s+/g, '-');
+      $('.'+neighborhood).css('fill', d3.interpolateBlues(d['Total 2012 Population']/100000))
+    }); // end forEach()
+  }); // end population csv
+}); // end neighborhood json
+
+/* Get averages for each population demographic */
+function getPopAvgs(data) {
+  var population = 0,
+      demographic = {'Black': '','Hispanic': '','Asian/Pacific Islander': '','Other Race/ Ethnicity': '','White': '', 'Total 2012 Population': ''},
+      count;
+
+  for(var [key, value] of Object.entries(demographic)) {
+    count = 0;
+    data.forEach(function(d) {
+      count += 1;
+      population += parseInt(d[key]);
     });
+
+    demographic[key] =
+      Math.round(population / count);
+  }
+
+  return demographic
+}
+
+function getPopMaxMin(data) {
+  var population = [], max, min;
+  data.forEach(function(d) {
+    population.push(parseInt(d['Total 2012 Population']));
   });
-});
+  max = population[0];
+  min = population[0];
+  
+  for(var i = 0; i < population.length; i++) {
+    if(population[i] > max) {max = population[i];}
+    if(population[i] < min) {min = population[i];}
+  }
+
+  return [max, min]
+}
+
+function colorScale(max, min, canvas) {
+  var x = d3.scaleLinear()
+    .domain([1, 10])
+    .rangeRound([10, 270]);
+
+  var color = d3.scaleThreshold()
+      .domain(d3.range(2, 10))
+      .range(d3.schemeBlues[9]);
+
+  var g = canvas.append("g")
+      .attr("class", "key")
+      .attr("transform", "translate(0,20)");
+
+  g.selectAll("rect")
+    .data(color.range().map(function(d) {
+        d = color.invertExtent(d);
+        if (d[0] == null) d[0] = x.domain()[0];
+        if (d[1] == null) d[1] = x.domain()[1];
+        return d;
+      }))
+    .enter().append("rect")
+      .attr("height", 8)
+      .attr("x", function(d) { return x(d[0]); })
+      .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+      .attr("fill", function(d) { return color(d[0]); });
+
+  g.append("text")
+      .attr("class", "caption")
+      .attr("x", x.range()[0])
+      .attr("y", -6)
+      .attr("fill", "#000")
+      .attr("text-anchor", "start")
+      .attr("font-weight", "bold")
+      .text("Population");
+
+  g.call(d3.axisBottom(x)
+      .tickSize(13)
+      .tickFormat(function(x, i) { return i ? x : x + "%"; })
+      .tickValues(color.domain()))
+    .select(".domain")
+      .remove();
+}
+
+function print(string) {
+  console.log(string)
+}
